cf98e9a020854c2ee2c459f07ee952de808546f6
akashjain132 <akashjain132@2622667.no-reply.drupal.org>
2014-09-06 09:19:49 +0530

    Add a ajax coding in custom form

--- js/custom_form_code_maker.js
+++ js/custom_form_code_maker.js
@@ -1,8 +1,19 @@
-(function (jQuery) {
+(function ($) {
 Drupal.behaviors.autosave_taxonomy = {
   attach: function (context, settings) {
     jQuery(document).ready(function() {
-
+      $('.ajax-element-field').change(function() {
+        if ($(this).is(':checked')) {
+    	  $(this).parents('fieldset').find('.ajax-callback-field').parents('.form-item').show();
+    	  $(this).parents('fieldset').find('.ajax-wrapper-field').parents('.form-item').show();
+    	  $(this).parents('fieldset').find('.ajax-method-field').parents('.form-item').show();
+    	}
+    	else {
+    	  $(this).parents('fieldset').find('.ajax-callback-field').parents('.form-item').hide();
+    	  $(this).parents('fieldset').find('.ajax-wrapper-field').parents('.form-item').hide();
+    	  $(this).parents('fieldset').find('.ajax-method-field').parents('.form-item').hide();
+    	}
+      });
     });
   }};
 
