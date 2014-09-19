ca18eed809eaeb2e2ef46639135e0d1ed5330aba
akashjain132 <akashjain132@2622667.no-reply.drupal.org>
2014-09-19 23:16:17 +0530

    Add field attributes fieldset

--- js/custom_form_code_maker.js
+++ js/custom_form_code_maker.js
@@ -4,15 +4,15 @@ Drupal.behaviors.autosave_taxonomy = {
     jQuery(document).ready(function() {
       $('.ajax-element-field').change(function() {
         if ($(this).is(':checked')) {
-    	  $(this).parents('fieldset').find('.ajax-callback-field').parents('.form-item').show();
-    	  $(this).parents('fieldset').find('.ajax-wrapper-field').parents('.form-item').show();
-    	  $(this).parents('fieldset').find('.ajax-method-field').parents('.form-item').show();
-    	}
-    	else {
-    	  $(this).parents('fieldset').find('.ajax-callback-field').parents('.form-item').hide();
-    	  $(this).parents('fieldset').find('.ajax-wrapper-field').parents('.form-item').hide();
-    	  $(this).parents('fieldset').find('.ajax-method-field').parents('.form-item').hide();
-    	}
+    	    $(this).parents('fieldset').find('.ajax-callback-field').parents('.form-item').show();
+    	    $(this).parents('fieldset').find('.ajax-wrapper-field').parents('.form-item').show();
+    	    $(this).parents('fieldset').find('.ajax-method-field').parents('.form-item').show();
+    	  }
+    	  else {
+    	    $(this).parents('fieldset').find('.ajax-callback-field').parents('.form-item').hide();
+    	    $(this).parents('fieldset').find('.ajax-wrapper-field').parents('.form-item').hide();
+    	    $(this).parents('fieldset').find('.ajax-method-field').parents('.form-item').hide();
+    	  }
       });
     });
   }};
